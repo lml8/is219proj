aa5ade5d3cb73fad52e0518519758c4217e77a37
lml8 <lml8@njit.edu>
2016-02-19 16:56:02 -0500

    proj1

--- index.php
+++ index.php
@@ -21,6 +21,31 @@
         /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
     }
     </style>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>Luis Leon | Web Designer |(for IS219)</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="resume1.css" media="all" />
+
+    <!-- Custom CSS -->
+    <style>
+    body {
+        padding-top: 02%;
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+    }
+    </style>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,16 +60,16 @@
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
+        <div class="container" id="main">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
+            <div class="navbar navbar-fixed-top"> 
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
+                <a class="navbar-brand" href="#">Welcome to My Page</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -53,7 +78,7 @@
                         <a href="#">About</a>
                     </li>
                     <li>
-                        <a href="#">Services</a>
+                        <a href="#">Achievements</a>
                     </li>
                     <li>
                         <a href="#">Contact</a>
@@ -61,34 +86,139 @@
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
+			<div class="carousel slide" id ="myCarousel">
+			</div>
         </div>
         <!-- /.container -->
     </nav>
+	
+	<div id="doc2" class="yui-t7">
+	<div id="inner">
+	
+		<div id="hd">
+			<div class="yui-gc">
+				<div class="yui-u first">
+					<h1>Luis Leon</h1>
+					<h3>Web Designer</h3>
+				</div>
 
-    <!-- Page Content -->
-    <div class="container">
+				<div class="yui-u">
+					<div class="contact-info">
+						<h4><a href="#">lml8@njit.edu</a></h4>
+						<h4>(313)-867-5300</h4>
+					</div><!--// .contact-info -->
+				</div>
+			</div><!--// .yui-gc -->
+		</div><!--// hd -->
 
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <h1>A Bootstrap Starter Template for IS219</h1>
-                <p class="lead">Complete with pre-defined file paths that you won't have to change!</p>
-                <ul class="list-unstyled">
-                    <li>Bootstrap v3.3.6</li>
-                    <li>jQuery v1.11.1</li>
-                </ul>
-            </div>
-        </div>
-        <!-- /.row -->
+		<div id="bd">
+			<div id="yui-main">
+				<div class="yui-b">
 
-    </div>
-    <!-- /.container -->
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Profile</h2>
+						</div>
+						<div class="yui-u">
+							<p>
+								A results driven, articulate and well presented associate with a proven track record of meeting store targets. Hardworking, able to communicate effectively with people form all backgrounds and able to present products in a structured Professional way face to face with customers. Comfortable working in a fast paced, growth orientated work environment and have experience of selling fast moving Consumer goods.
+							</p>
+						</div>
+					</div><!--// .yui-gf -->
 
-    <!-- jQuery Version 1.11.1 -->
-    <script src="js/jquery.js"></script>
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Skills</h2>
+						</div>
+						<div class="yui-u">
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+								<div>
+									<h3>Web Design</h3>
+									<p>Assertively exploit wireless initiatives rather than synergistic core competencies.	</p>
+								</div>
+
+								<div>
+									<h3>Bilingual</h3>
+									<p>Fluent in both English and Spanish. </p>
+								</div>
+								
+								<div>
+									<h3>Interpersonal</h3>
+									<p>A active listener able to process problems of cutomers and be able to come up with a soultion in little or no time. </p>
+								</div>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Technical</h2>
+						</div>
+						<div class="yui-u">
+							<ul class="talent">
+								<li>XHTML</li>
+								<li>CSS</li>
+								<li class="last">Javascript</li>
+							</ul>
+
+							<ul class="talent">
+								<li>Jquery</li>
+								<li>PHP</li>
+								<li class="last">Python</li>
+							</ul>
+
+						</div>
+					</div><!--// .yui-gf-->
+
+					<div class="yui-gf">
+	
+						<div class="yui-u first">
+							<h2>Experience</h2>
+						</div><!--// .yui-u -->
+
+						<div class="yui-u">
+
+							<div class="job">
+								<h3>Radio Shack</h3>
+								<h3>Sales Associate</h3>
+								<h3>2013-2016</h3>
+								<p>Enable optimal customer statisfaction through interactive relationships. Phosfluorescently implement worldwide vortals and client-focused imperatives. Conveniently initiate virtual paradigms and top-line convergence. </p>
+							</div>
+
+							<div class="job">
+								<h3>Taco Bell</h3>
+								<h3>Team Member</h3>
+								<h3>2012-2013</h3>
+								<p>Implement all the standard operational procedures of the company and make sure that every customer receives the best customer service possible.. </p>
+							</div>
+
+
+						</div><!--// .yui-u -->
+					</div><!--// .yui-gf -->
+
+
+					<div class="yui-gf last">
+						<div class="yui-u first">
+							<h2>Education</h2>
+						</div>
+						<div class="yui-u">
+							<h3>New Jersey Institute of Technology &mdash; Newark, NJ</h3>
+							<h4>Major, Web & Information System</h4>
+						</div>
+					</div><!--// .yui-gf -->
+
+
+				</div><!--// .yui-b -->
+			</div><!--// yui-main -->
+		</div><!--// bd -->
+
+		<div id="ft">
+			<p>Luis Leon&mdash; <a href="#">lml8@njit.edu</a> &mdash; (313) - 867-5300</p>
+		</div><!--// footer -->
+
+	</div><!-- // inner -->
+
+
+</div><!--// doc -->
+
 
 </body>
-
-</html>
